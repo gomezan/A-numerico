30851089ea923011476af59156f0016e4dacdddb
gomezan <gomezan@javeriana.edu.co>
2018-02-26 23:15:01 -0500

    preparcial

--- canvas.R
+++ canvas.R
@@ -1,38 +1,26 @@
 rm(list=ls())
 # convierte los numeros binarios a decimales
-decimal <- function(a,b) {
+pflotante <- function(a,b) {
 tam<-length(a)
-#Parte entera
-j<-0
-sum<-0
-res<-0
-while(0<tam)
-{
-  res<-((a[c(tam)]))
-  if(res!=0)
-  {
-    sum=sum+(res*2^j) 
-  }
-  tam<-tam-1
-  j<-j+1
-}
-#cat(sum,".")
-#parte decimal
 tam<-length(b)
+pos=1
+if((tam<2)&&(a[c(1)]==0))
+{
+  while(true)
+  if((a[c(pos)])==1)
+  {
+    
+  }
+  pos=pos+1
+}
+
 j<-1
 while((j<=tam))
 {
-  res<-(b[c(j)])
-  if(res!=0)
-  {
-    sum=sum+(res*2^((j)*(-1))) 
-  }
-  j<-j+1
+
 }
 cat(sum)
 }
-a<-c(1,1,1)
-b<-c(1,1,1,1,1,1)
-decimal(a,b)
+pflotante(a,b)
 
 
