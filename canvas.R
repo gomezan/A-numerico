a9a2885884733a7495b8d40a6c2bf5f13b67838f
gomezan <gomezan@javeriana.edu.co>
2018-02-27 22:13:26 -0500

    preparcial

--- canvas.R
+++ canvas.R
@@ -1,26 +1,4 @@
 rm(list=ls())
-# convierte los numeros binarios a decimales
-pflotante <- function(a,b) {
-tam<-length(a)
-tam<-length(b)
-pos=1
-if((tam<2)&&(a[c(1)]==0))
-{
-  while(true)
-  if((a[c(pos)])==1)
-  {
-    
-  }
-  pos=pos+1
-}
-
-j<-1
-while((j<=tam))
-{
-
-}
-cat(sum)
-}
-pflotante(a,b)
-
-
+as.double("123456789123456789")
+class(as.double("123456789123456789"))
+is.double(as.double("123456789123456789"))
